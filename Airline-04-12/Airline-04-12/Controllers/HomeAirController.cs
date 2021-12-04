ddf059b2673bd654ee8b443811609d59ceb271ba
Huỳnh Trần Thế Hùng <hunghttts2008029@fpt.edu.vn>
2021-12-04 23:51:49 +0700

    update layout

--- Airline-04-12/Airline-04-12/Controllers/HomeAirController.cs
+++ Airline-04-12/Airline-04-12/Controllers/HomeAirController.cs
@@ -16,21 +16,21 @@ namespace Airline_04_12.Controllers
         {
             this.ctx = ctx;
         }
-       
+
         public async Task<IActionResult> Index()
-        {           
+        {
             return View(await ctx.Tickets.ToListAsync());
         }
+
         public async Task<IActionResult> Searchtiket()
         {
             return View(await ctx.Tickets.ToListAsync());
         }
         [HttpPost]
-<<<<<<< HEAD
         public async Task<IActionResult> Searchtickets(string departureaddress, string arrivalAddress, DateTime time)
         {
             var result = await ctx.Tickets.Where(t => t.DepartureAddress.Contains(departureaddress) && t.ArrivalAddress.Contains(arrivalAddress) && t.DepartureDate == time && t.Status == 1).ToListAsync();
-            if(result != null)
+            if (result != null)
             {
                 return View("Index", result);
             }
@@ -38,12 +38,8 @@ namespace Airline_04_12.Controllers
             {
                 return View();
             }
-=======
-        public async Task<IActionResult> Searchtickets(string departureaddress, string arrivalAddress, string time)
-        {
-            var result = await ctx.Tickets.Where(t => t.DepartureAddress.Contains(departureaddress) && t.ArrivalAddress.Contains(arrivalAddress) && t.DepartureDate.Contains(time)).ToListAsync();
-            return View("Index",result);
->>>>>>> 96ee2d874c86eacc15bbb94b79bfa47d4b99ce69
         }
+        
     }
 }
+
