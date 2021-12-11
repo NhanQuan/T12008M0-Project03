e2cd8812a9b0614996d15f076935b4a6de38686a
NhanQuan <Nhannvts2008014@fpt.edu.vn>
2021-12-11 14:12:32 +0700

    up

--- BanVeMayBay/BanVeMayBay/Models/ticket.cs
+++ BanVeMayBay/BanVeMayBay/Models/ticket.cs
@@ -14,15 +14,19 @@ namespace BanVeMayBay.Models
         public int id { get; set; }
         [Required(ErrorMessage = "Ticket is not blank")]
         public string name { get; set; }
+        [Required(ErrorMessage = "Description is not blank")]
         public string description { get; set; }
         public string flightCode { get; set; }
+        
         public string airline { get; set; }
+        [Required(ErrorMessage = "Airport is not blank")]
         public string airport { get; set; }
         public string ticketType { get; set; }
         public string img { get; set; }      
         public string departure_address { get; set; }
         public string arrival_address { get; set; }
         public DateTime departure_date { get; set; }
+        [Required(ErrorMessage = "Seats is not blank"), Range(50, int.MaxValue, ErrorMessage = "Please enter a value bigger than {50}")]
         public int? guestTotal { get; set; }
         [Required(ErrorMessage = "Price is not blank"), Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public double price { get; set; }
